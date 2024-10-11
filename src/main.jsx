920ba99cada01780cc1333239cf2cfd42332f50a
Najaf Ali Balti <najaf.balti000@gmail.com>
2024-10-11 14:04:33 +0500

    Just resolved all issues about setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+import { Provider } from 'react-redux';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from './store.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
--- src/store.js
+++ src/store.js
@@ -1,2 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
 export const store = configureStore({
+    reducer: {
+        
+    }
 })
+

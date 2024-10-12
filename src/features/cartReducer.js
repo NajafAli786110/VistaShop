339f4d5ca92fa50557a73887c05977809456e04b
Najaf Ali Balti <najaf.balti000@gmail.com>
2024-10-12 13:45:37 +0500

    Implemented Local Storage Funcitonality

--- src/features/cartReducer.js
+++ src/features/cartReducer.js
@@ -1,13 +1,23 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-export const initialStateCart = [
-  {
-    id: 0,
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-    name: "Hello",
-    price: 0,
-  },
-];
+const loadFromLocalStorage = () => {
+  const savedCartItems = localStorage.getItem("initialStateCart");
+  if (savedCartItems) {
+    return JSON.parse(savedCartItems);
+  } else {
+    return [
+      {
+        id: 0,
+        image:
+          "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+        name: "Hello",
+        price: 0,
+      },
+    ];
+  }
+};
+
+export const initialStateCart = loadFromLocalStorage();
 
 const cartReducer = createSlice({
   name: "cartItems",
@@ -21,15 +31,23 @@ const cartReducer = createSlice({
         price: action.payload.price,
       };
       state.push(newItem);
+
+      localStorage.setItem("initialStateCart", JSON.stringify(state));
     },
     CART_REMOVE_ITEM: (state, action) => {
-      return state.filter((items) => items.id != action.payload.id);
+      const updatedState = state.filter(
+        (item) => item.id !== action.payload.id
+      );
+      localStorage.setItem("initialStateCart", JSON.stringify(updatedState));
+      return updatedState;
     },
-    CART_FLASH: () => {
+    CART_FLASH: (state) => {
+      localStorage.setItem("initialStateCart", JSON.stringify([]));
       return [];
     },
   },
 });
 
-export const { CART_ADD_ITEM, CART_FLASH, CART_REMOVE_ITEM } = cartReducer.actions;
-export default cartReducer.reducer
+export const { CART_ADD_ITEM, CART_FLASH, CART_REMOVE_ITEM } =
+  cartReducer.actions;
+export default cartReducer.reducer;
